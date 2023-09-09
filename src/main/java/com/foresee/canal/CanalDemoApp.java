d19c691f006390a14643529c6d049bf8064b98e1
yuanrui01 <xdyrfree@gmail.com>
2023-09-09 13:32:19 +0800

    canal demo commit

---
+++ src/main/java/com/foresee/canal/CanalDemoApp.java
@@ -0,0 +1,16 @@
+package com.foresee.canal;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther zzyy
+ * @create 2022-12-23 11:59
+ */
+@SpringBootApplication
+public class CanalDemoApp
+{
+    public static void main(String[] args)
+    {
+        //SpringApplication.run(CanalDemo02App.class,args);
+    }
+}
